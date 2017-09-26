a9398f668861e791351862ef3b56f61609f14f06
Dennis Alund <dennis.alund@gmail.com>
2017-09-26 09:05:12 +0800

    Casting env variables to bool

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import * as app from "./app";
 
 app.hotspot.set('nexudus_space_name', functions.config().nexudus.shortname);
 app.hotspot.set('redirect_url', functions.config().unifi.redirect_url);
-app.hotspot.set('unifi_use_ssl', functions.config().unifi.use_ssl);
-app.hotspot.set('unifi_ssl_is_self_signed', functions.config().unifi.is_selfsigned);
+app.hotspot.set('unifi_use_ssl', !!functions.config().unifi.use_ssl);
+app.hotspot.set('unifi_ssl_is_self_signed', !!functions.config().unifi.is_selfsigned);
 app.hotspot.set('unifi_host', functions.config().unifi.host);
 app.hotspot.set('unifi_port', functions.config().unifi.port);
 app.hotspot.set('unifi_username', functions.config().unifi.username);
